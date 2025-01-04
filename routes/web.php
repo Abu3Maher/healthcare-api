57a3b0d5459f94521fc78ed1cd6fabe39d357b88
AbuMaher <101367311+Abu3Maher@users.noreply.github.com>
2025-01-04 20:08:17 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
-
-require __DIR__.'/auth.php';
+Route::post('auth/login', LoginController::class)
+    ->name('login');
