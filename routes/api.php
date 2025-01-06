fbd8d1618a22a24b6394b82a08335a926f87a8e5
AbuMaher <101367311+Abu3Maher@users.noreply.github.com>
2025-01-06 22:25:38 +0200

    Update the route place

--- routes/api.php
+++ routes/api.php
@@ -6,33 +6,6 @@ use App\Http\Controllers\Doctor\DoctorController;
 use App\Http\Controllers\Service\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::name('api.')->group(function () {
-
-        Route::get('/user', UserController::class)->name('user');
-
-        Route::name('services.')->prefix('/services/')->group([function () {
-            Route::get('', [ServiceController::class, 'index'])->name('index');
-            Route::post('store', [ServiceController::class, 'store'])->name('store');
-            Route::get('{id}/edit', [ServiceController::class, 'edit'])->name('edit');
-            Route::post('{id}/update', [ServiceController::class, 'update'])->name('update');
-            Route::delete('{id}/delete', [ServiceController::class, 'delete'])->name('delete');
-
-        }]);
-
-        Route::name('doctors.')->prefix('/doctors/')->group(function () {
-            Route::get('', [DoctorController::class, 'index'])->name('index');
-            Route::get('{id}/edit', [DoctorController::class, 'edit'])->name('edit');
-            Route::post('{id}/update', [DoctorController::class, 'update'])->name('update');
-
-        });
-
-        Route::name('appointments.')->prefix('/appointments/')->group(function () {
-            Route::get('', [AppointmentController::class, 'index'])->name('index');
-            Route::get('create', [AppointmentController::class, 'create'])->name('create');
-            Route::post('store', [AppointmentController::class, 'store'])->name('store');
-            Route::post('{id}/update', [AppointmentController::class, 'update'])->name('update');
-
-        });
-    });
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,49 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Appointment\AppointmentController;
+use App\Http\Controllers\Constants\UserController;
+use App\Http\Controllers\Doctor\DoctorController;
+use App\Http\Controllers\Service\ServiceController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', LoginController::class)
-    ->name('login');
+Route::name('auth.')->prefix('/auth/')->group(function () {
 
-Route::post('register', RegisterController::class)
-    ->name('register');
+    Route::post('login', LoginController::class)
+        ->name('login');
 
-Route::post('logout', LogoutController::class)
-    ->name('logout');
+    Route::post('register', RegisterController::class)
+        ->name('register');
 
+    Route::post('logout', LogoutController::class)
+        ->name('logout');
+});
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+
+    Route::get('/user', UserController::class)->name('user');
+
+    Route::name('services.')->prefix('/services/')->group([function () {
+        Route::get('', [ServiceController::class, 'index'])->name('index');
+        Route::post('store', [ServiceController::class, 'store'])->name('store');
+        Route::get('{id}/edit', [ServiceController::class, 'edit'])->name('edit');
+        Route::post('{id}/update', [ServiceController::class, 'update'])->name('update');
+        Route::delete('{id}/delete', [ServiceController::class, 'delete'])->name('delete');
+
+    }]);
+
+    Route::name('doctors.')->prefix('/doctors/')->group(function () {
+        Route::get('', [DoctorController::class, 'index'])->name('index');
+        Route::get('{id}/edit', [DoctorController::class, 'edit'])->name('edit');
+        Route::post('{id}/update', [DoctorController::class, 'update'])->name('update');
+
+    });
+
+    Route::name('appointments.')->prefix('/appointments/')->group(function () {
+        Route::get('', [AppointmentController::class, 'index'])->name('index');
+        Route::get('create', [AppointmentController::class, 'create'])->name('create');
+        Route::post('store', [AppointmentController::class, 'store'])->name('store');
+        Route::post('{id}/update', [AppointmentController::class, 'update'])->name('update');
+
+    });
+});
